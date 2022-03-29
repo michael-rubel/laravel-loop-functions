4d282c435d32e588f0cff398b3e59985904fa675
Michael Rubel <contact@observer.name>
2022-03-29 15:11:21 +0200

    add tests for dumpProperties

---
+++ tests/RecursiveClassDumpTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MichaelRubel\LoopFunctions\Tests;
+
+use Illuminate\Support\Collection;
+use MichaelRubel\LoopFunctions\Tests\Boilerplate\TestClass;
+
+class RecursiveClassDumpTest extends TestCase
+{
+    /** @test */
+    public function testCanDumpClassProperties()
+    {
+        $properties = app(TestClass::class)->dumpProperties();
+
+        $this->assertTrue($properties['bool']);
+        $this->assertStringContainsString( 'new string', $properties['string']);
+        $this->assertArrayHasKey('array', $properties['array']);
+        $this->assertTrue($properties['collection']->isEmpty());
+        $this->assertArrayHasKey('static', $properties['static']);
+
+        $this->assertNull($properties['nullable_string']);
+        $this->assertNull($properties['nullable_array']);
+        $this->assertNull($properties['nullable_collection']);
+        $this->assertNull($properties['nullable_static_array']);
+    }
+
+    /** @test */
+    public function testCanDumpPassedObjectProperties()
+    {
+        $properties = app(TestClass::class)->dumpProperties(Collection::class);
+
+        $this->assertArrayHasKey('items', $properties);
+        $this->assertArrayHasKey('escapeWhenCastingToString', $properties);
+        $this->assertFalse($properties['escapeWhenCastingToString']);
+    }
+}
